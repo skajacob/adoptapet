30c8c83732463dea3863dfa7169c29e71b8fb6f2
Jacob Muñoz <jmg_extreme@hotmail.com>
2022-02-02 22:07:28 -0600

    fisrt commit

---
+++ config/env.sh
@@ -0,0 +1,4 @@
+export NODE_ENV='development'
+export PORT=3000
+export SECRET='secret' # para mayor seguridad puedes cambiar esto por el secreto de tu preferencia
+export MONGODB_URI='mongodb+srv://skajacob:qwerty1234@cluster0.que8m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
